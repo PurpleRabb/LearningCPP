4b6253a148d2da3132a514e8ec3603f6623031c4
liushuo <liushuo@kedacom.com>
2019-08-21 04:04:19 -0400

    二元运算符重载
    1. +  重载
    2. << 重载
    3. [] 重载

--- operator/Coordinate.h
+++ operator/Coordinate.h
@@ -5,6 +5,8 @@ using namespace std;
 
 class Coordinate {
 	friend Coordinate &operator-(Coordinate &coor); //友元函数一元运算符重载
+	friend Coordinate operator+(Coordinate &c1,Coordinate &c2); //友元函数二元运算符重载
+	friend ostream &operator<<(ostream &output,Coordinate &coor); //第一个参数必须是ostream，所以只能用友元函数重载
 	public:
 		Coordinate(int x,int y):m_X(x),m_Y(y) {
 
@@ -32,8 +34,31 @@ class Coordinate {
 			return old;
 		}
 
+		/*Coordinate operator+(const Coordinate &coor) { //成员函数二元运算符重载
+			Coordinate temp(0,0);
+			temp.m_X = this->m_X + coor.m_X;
+			temp.m_Y = this->m_Y + coor.m_Y;
+			return temp;
+		}*/
+
+		int operator[](int index) { //索引运算符重载，必须是成员函数
+			switch(index) {
+				case 1:
+				return m_X;
+				case 2:
+				return m_Y;
+				default:
+				return -1;
+			}
+			return -1;
+		}
+
 		int getY() {
 			return m_Y;
+		}
+
+		int getX() {
+			return m_X;
 		}
 
 		void printInfo() {
--- operator/main.cpp
+++ operator/main.cpp
@@ -8,11 +8,27 @@ Coordinate &operator-(Coordinate &coor) {
 	return coor;
 }
 
+Coordinate operator+(Coordinate &coor1,Coordinate &coor2) {
+	Coordinate temp(0,0);
+	temp.m_X = coor1.m_X + coor2.m_X;
+	temp.m_Y = coor1.m_Y + coor2.m_Y;
+	return temp;
+}
+
+ostream& operator<<(ostream &output,Coordinate &coor) {
+	output << coor.m_X << "#,#" << coor.m_Y << endl;
+	return output;
+}
+
 int main() {
-	Coordinate coor(10,20);
-	-coor; //相当于coor.operator-();
+	Coordinate coor1(10,20);
+	Coordinate coor2(10,20);
+	//-coor; //相当于coor.operator-();
 	//cout << (coor++).getY() << endl;
-	cout << (++coor).getY() << endl;
-	coor.printInfo();
+	//cout << (++coor).getY() << endl;
+	Coordinate newCoor = coor1 + coor2;
+	cout << coor1;
+	cout << "索引访问：" << newCoor[1] << endl;
+	newCoor.printInfo();
 }
 
